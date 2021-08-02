523f899d9e67d06c3f75d6cdafb113a100ce06bb
u.aleroev <u.aleroev@corp.mail.ru>
2021-08-02 17:46:29 +0300

    chore: add dto factory contracts/traits

---
+++ app/Versions/V1/DTO/Traits/HasFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Versions\V1\DTO\Traits;
+
+use App\Versions\V1\Factories\DTO\FactoryInterface;
+
+trait HasFactory
+{
+    public static function factory(): FactoryInterface
+    {
+        return new static::$factory();
+    }
+}
