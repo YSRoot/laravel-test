51001349755dedff07b8e280692b64c94d45c296
u.aleroev <u.aleroev@corp.mail.ru>
2021-08-03 13:38:07 +0300

    chore: psr cleanup

--- app/Versions/V1/Facades/OAuth.php
+++ app/Versions/V1/Facades/OAuth.php
@@ -6,7 +6,6 @@ use App\Versions\V1\DTO\OAuthAuthorizeContract;
 use App\Versions\V1\Services\Auth\TokenManagers\TokenManagerInterface;
 use Illuminate\Support\Facades\Facade;
 
-
 /**
  * @method static array make(OAuthAuthorizeContract $dto)
  * @method static TokenManagerInterface driver(string $driver)
